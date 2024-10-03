1303c6d486ae1bf865725c5d62be82e2b5c59bfb
Roman <roman.mitsura@mail.ru>
2024-10-03 17:23:19 +0300

    users

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express"; //можно писать ES6 код благодаря "type": "module", в package.json
 import jwt from "jsonwebtoken"; //импорт библиотеки jwt
+import bcrypt from "bcrypt"; //импорт библиотеки bcrypt
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
 
-import second from "./validations/auth.js";
+import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/User.js"; //Импортируем модель пользователя
 
 mongoose
   .connect(
-    "mongodb+srv://admin:wwwwww@cluster0.63ays.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://admin:wwwwww@cluster0.63ays.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("DB ok");
@@ -20,7 +24,30 @@ const app = express(); // создание express приложения. Вся 
 app.use(express.json()); //позволяет читать json который к нам приходит
 
 //когда мне придет запрос на этот адресс "/auth/login" я хочу отловить запрос, вытащить запрос и ответ
-app.post("/auth/register", (req, res) => {});
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req); //Проверяем ошибки из запроса (req)
+    if (!errors.isEmpty()) {
+      //Проверить если есть ошибки то вернуть статут 400 и все ошибки
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      passwordHash,
+      avatarUrl: req.body.avatarUrl,
+    });
+
+    const user = await doc.save(); //Создание пользователя. Резульатт вернувшийся от mongo db передается user
+
+    res.json(user); //Возвращаем пользователю информацию о пользователе
+  } catch (err) {}
+});
 
 //запуск приложения по порту 4444. Функция для обработки ошибки
 app.listen(4444, (err) => {
--- validations/auth.js
+++ validations/auth.js
@@ -1,9 +1,11 @@
 import { body } from "express-validator";
 
-export const refisterValidation = [
-  body("email").isEmail(), //если email коректный то пропускаем
-  body("password").isLength({ min: 5 }),
-  body("fullName").isLength({ min: 2 }),
-  body("avatarUrl").optional().isURL(), //Опционально. Если не придет то ничего страшного,
+export const registerValidation = [
+  body("email", "Неверный формат почты").isEmail(), //если email коректный то пропускаем
+  body("password", "Пароль должен быть минимум 5 символов").isLength({
+    min: 5,
+  }),
+  body("fullName", "Укажите имя (минимум 2 символа)").isLength({ min: 2 }),
+  body("avatarUrl", "Неверная ссылка на аватарку").optional().isURL(), //Опционально. Если не придет то ничего страшного,
   //если пришло то проверить URL ли это
 ];
